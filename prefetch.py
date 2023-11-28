1f945fa302a962b6f84b627cae3d4f8ee8f776a8
parsnip908 <pkharche@ucdavis.edu>
2023-11-28 02:47:28 -0600

    Update prefetch.py

--- prefetch.py
+++ prefetch.py
@@ -12,21 +12,21 @@ num_procs = int(sys.argv[2])
 
 print(f"Reading file from: {trace_path} num procs: {num_procs}")
 
-# pagesize - standard 4KB
+page_size = 12 # standard 4KB
 # number of frames in memory
 # wait time for accurate prefetch
 # logical cycles upper limit
 nodes = []
 
 class Request:
-    def __init__(self, ln):
-        tk = ln.split(",")
-        self.lamport_clk = int(tk[0])
-        self.id = int(tk[1])
-        self.rw = tk[2]
-        self.addr = int(tk[3], 0)
-        self.size = int(tk[4])
-        self.type = 0
+    # def __init__(self, ln):
+    #     tk = ln.split(",")
+    #     self.lamport_clk = int(tk[0])
+    #     self.id = int(tk[1])
+    #     self.rw = tk[2]
+    #     self.addr = int(tk[3], 0)
+    #     self.size = int(tk[4])
+    #     self.type = 0
 
     def __init__(self, lamport_clk_, id_, rw_, addr_, size_, type_):
         self.lamport_clk = lamport_clk_
@@ -35,6 +35,15 @@ class Request:
         self.addr = addr_
         self.size = size_
         self.type = type_
+
+    def __str__(self):
+        return "Request:" + \
+                "\nclk:  " + str(self.lamport_clk) + \
+                "\nid:   " + str(self.id) + \
+                "\nrw:   " + str(self.rw) + \
+                "\naddr: " + hex(self.addr) + \
+                "\nsize: " + str(self.size) + \
+                "\ntype: " + str(self.type)
 
     def get_all(self):
         return {"clk": self.lamport_clk, 
@@ -66,15 +75,17 @@ class Trace:
         self.count = 0
         file = open(path, "r")
         file_lines = file.read().splitlines()
-        for ln in self.file_lines:
-            self.trace.append(Request(ln))
+        print('generating trace')
+        for ln in file_lines:
+            tk = ln.split(",")
+            self.trace.append(Request(int(tk[0]), int(tk[1]), tk[2], int(tk[3], 0), int(tk[4]), 0))
             self.count += 1
-        close(file)
+        file.close()
 
     def pop_request(self):
         if(self.count):
             self.count -= 1
-            return trace.pop(0)
+            return self.trace.pop(0)
         return None
 
     def get_count(self):
@@ -100,9 +111,11 @@ class Prefetcher:
 class PTE:
     def __init__(self, num):
         self.page_num = num
-        self.home_node = page_num && 0x0f
+        self.home_node = num % num_procs
         self.curr_nodes = [home_node]
-        self.permissions = 0
+        self.state = 0
+        self.evict_cause = None
+
 
 class Page_table:
     def __init__(self, pid_):
@@ -113,23 +126,21 @@ class Page_table:
         pass
 
 class Node:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, id_):
+        self.id = id_
         self.prefetcher = Prefetcher()
         self.metric = Metrics()
-        self.page_table
+        self.page_table = Page_table(self.id)
         self.request_queue = []    
 
     def service_request(self, request):
         addr = request.get_addr()
         page_num = addr >> page_size
         home_node = page_num % num_procs
-        
-
         # Process access
         # See if hit or miss
         # update the prefetcher
-        Prefetcher.update(request)
+        self.prefetcher.update(request)
         return 0
 
     def add_request(self, req):
@@ -137,11 +148,15 @@ class Node:
 
 
 trace = Trace(trace_path)
+print(trace.get_count())
+print(trace.trace[0])
+original_count = trace.count
 
 for i in range(num_procs):
     nodes.append(Node(i))
 
 # core loop
+print('core loop')
 clk = 0
 curr_request = trace.pop_request()
 while trace.get_count() > 0: # need upper limit for cycles as a safety
@@ -164,3 +179,5 @@ while trace.get_count() > 0: # need upper limit for cycles as a safety
             nodes[i].service_request(prefetch_req)
 
     clk += 1
+    if clk % 5000 == 0:
+        print("reached clk " + str(clk) + " with count " + str(trace.get_count()))
